eb288629104c2b63354398c334318d430e9b5538
himprover <hwang.improver@gmail.com>
2021-08-22 01:19:21 +0900

    part 2 start

--- src/utils/useNavigation.tsx
+++ src/utils/useNavigation.tsx
@@ -1,6 +1,15 @@
-import {useContext} from 'react'
-import {NavigationScreenProp, NavigationRoute, NavigationContext, NavigationParams, NavigationRouter} from 'react-navigation'
+import { useContext } from 'react';
+import {
+	NavigationScreenProp,
+	NavigationRoute,
+	NavigationContext,
+	NavigationParams,
+	NavigationRouter,
+} from 'react-navigation';
 
 export function useNavigation() {
-    return useContext(NavigationContext) as NavigationScreenProp<NavigationRoute, NavigationParams>
+	return useContext(NavigationContext) as NavigationScreenProp<
+		NavigationRoute,
+		NavigationParams
+	>;
 }
